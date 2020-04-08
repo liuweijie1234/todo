5ef49760d9d5ebfa5cc34869ab91953b24a0f1d2
TENCENT\v_awjliu <v_awjliu@tencent.com>
2020-04-08 21:36:43 +0800

    1

--- config/dev.py
+++ config/dev.py
@@ -27,9 +27,9 @@ DEBUG = True
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': APP_CODE,
+        'NAME': 'todo',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': '123456',
         'HOST': 'localhost',
         'PORT': '3306',
     },
