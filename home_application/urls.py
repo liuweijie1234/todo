4606dc0993384cff60c1769735b5799165f51405
TENCENT\v_awjliu <v_awjliu@tencent.com>
2020-04-26 16:23:37 +0800

    1

--- home_application/urls.py
+++ home_application/urls.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
 
 from django.conf.urls import url
-
 from . import views
 
 urlpatterns = (
-    url(r'^$', views.home),
-    url(r'^dev-guide/$', views.dev_guide),
-    url(r'^contact/$', views.contact),
+    url(r'^$', views.tasks),
+    url(r'^tasks/$', views.tasks),
+    url(r'^record/$', views.record),
 )
--- home_application/views.py
+++ home_application/views.py
@@ -1,25 +1,18 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
-
+from .models import SelectScript
 
 # 开发框架中通过中间件默认是需要登录态的，如有不需要登录的，可添加装饰器login_exempt
 # 装饰器引入 from blueapps.account.decorators import login_exempt
-def home(request):
-    """
-    首页
-    """
-    return render(request, 'home_application/index_home.html')
 
 
-def dev_guide(request):
-    """
-    开发指引
-    """
-    return render(request, 'home_application/dev_guide.html')
+def tasks(request):
+    tasks = SelectScript.objects.all()
+    data = {"tasks": tasks}
+    return render(request, 'home_application/tasks.html', data)
 
 
-def contact(request):
-    """
-    联系页
-    """
-    return render(request, 'home_application/contact.html')
+def record(request):
+    return render(request, 'home_application/record.html')
+
+
